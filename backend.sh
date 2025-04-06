6f6f418fe47b8696d3379951b7708b0409408b4a
ramanji4 <ramanji181818@gmail.com>
2025-04-06 18:56:43 +0530

    backend

--- backend.sh
+++ backend.sh
@@ -2,7 +2,7 @@
 
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIME_STAMP=$(+%Y-%m-%d-%H-%M-%S)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
 mkdir -p $LOGS_FOLDER
 
