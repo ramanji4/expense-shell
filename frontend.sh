897b1370f3c7f43a04abd05cf3bdc8f0d7119441
ramanji4 <ramanji181818@gmail.com>
2025-04-06 19:03:27 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -4,7 +4,7 @@ USERID=$(id -u)
 
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
-TIME_STAMP=$(+%Y-%m-%d-%H-%M-%S)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
 SCRIPT_NAME=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
 mkdir -p $LOGS_FOLDER
 
@@ -12,3 +12,57 @@ R="\e[31m"
 G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
+
+ROOT_ACCESS(){
+    if [ $USERID -ne 0 ]
+    then
+        echo -e "$R PLEASE RUN THE SCRIPT WITH ROOT PRIVELEGES $N" | tee -a $LOG_FILE
+        exit 1
+    fi
+}
+
+VALIDATION(){
+    if [ $1 -ne 0 ]
+    then
+        echo -e "$R...is FAILED...Please cehck it once $N" | tee -a $LOG_FILE
+        exit 1
+    else
+        echo -e "$G...is SUCCESS $N" | tee -a $LOG_FILE
+    fi
+}
+
+echo "Script started executing at: $(date)" | tee -a $LOG_FILE
+
+ROOT_ACCESS
+
+dnf list installed nginx &>>$LOG_FILE
+if [ $? -ne 0 ]
+then
+    echo -e "$Y nginx is not installed...going to install $N" | tee -a $LOG_FILE
+    dnf install nginx -y &>>$LOG_FILE
+    VALIDATION $? "installing nginx"
+else
+    echo -e "$G nginx is already installed...$N" | tee -a $LOG_FILE
+fi
+
+systemctl enable nginx &>>$LOG_FILE
+VALIDATION $? "Enabling nginx"
+
+systemctl start nginx &>>$LOG_FILE
+VALIDATION $? "Starting nginx"
+
+rm -rf /usr/share/nginx/html/* &>>$LOG_FILE
+VALIDATION $? "Removing default content" 
+
+curl -o /tmp/frontend.zip https://expense-builds.s3.us-east-1.amazonaws.com/expense-frontend-v2.zip &>>$LOG_FILE
+VALIDATION $? "Downloading frontend code"
+
+cd /usr/share/nginx/html
+unzip /tmp/frontend.zip &>>$LOG_FILE
+VALIDATION $? "Extracting frontend code"
+
+cp /home/ec2-user/expense-shell/expense.conf /etc/nginx/default.d/expense.conf
+VALIDATION $? "Copying expense conf" 
+
+systemctl restart nginx &>>$LOG_FILE
+VALIDATION $? "Restarting nginx" 
