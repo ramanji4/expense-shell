3868f3bd126ec0648e18941c19dd64aa63161deb
ramanji4 <ramanji181818@gmail.com>
2025-04-06 19:08:37 +0530

    frontend

--- frontend.sh
+++ frontend.sh
@@ -5,7 +5,7 @@ USERID=$(id -u)
 LOGS_FOLDER="/var/log/expense"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
-SCRIPT_NAME=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
+LOG_FILE=$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log
 mkdir -p $LOGS_FOLDER
 
 R="\e[31m"
